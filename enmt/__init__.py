039f8d18ee46e504a9fd28cfb9053ec0374a005f
Marek Petrovič <32146793+marekninja@users.noreply.github.com>
2022-02-08 14:28:49 +0100

    support logs on many datasets

--- enmt/__init__.py
+++ enmt/__init__.py
@@ -4,3 +4,4 @@ from .results import Dataset, Pipeline, Scenario
 from .quant_helper import QuantizationMode
 from .dataset import Dataset
 from .qat_trainer import QatTrainer, QatTrainingArgs
+from .callbacks import RobustCallback
