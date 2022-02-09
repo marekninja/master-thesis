1d013cd5fceca6a55339c76a5f1df00a5964bb96
Marek Petrovič <32146793+marekninja@users.noreply.github.com>
2022-02-09 12:56:33 +0100

    train params

--- runnerTrainFP.py
+++ runnerTrainFP.py
@@ -38,12 +38,16 @@ modelFP.reset()
 train = OpenSubtitles(test_size=0.1, seed=42)
 # train = EuroParl(test_size=0.1, seed=42)
 
+batch_size = 16
+grad_acc_steps = 4
+train_epochs = 8
+
 training_args = {'metric_for_best_model': "eval_bleu", 'greater_is_better': True, "load_best_model_at_end": True,
                      "save_strategy": "steps",
                      'evaluation_strategy': 'steps', "save_steps": 2000, "eval_steps": 2000, 'logging_first_step': True,
-                     'learning_rate': 2e-5, 'per_device_train_batch_size': 8, 'gradient_accumulation_steps': 8,
-                     'per_device_eval_batch_size': 4, 'weight_decay': 0.01, 'save_total_limit': 3,
-                     'num_train_epochs': 8, 'predict_with_generate': True, 'no_cuda': False,
+                     'learning_rate': 2e-5, 'per_device_train_batch_size': batch_size, 'gradient_accumulation_steps': grad_acc_steps,
+                     'per_device_eval_batch_size': batch_size, 'weight_decay': 0.01, 'save_total_limit': 3,
+                     'num_train_epochs': train_epochs, 'predict_with_generate': True, 'no_cuda': False,
                      'fp16': False, 'push_to_hub': False,
                      'disable_tqdm': True,
                      }
@@ -69,7 +73,7 @@ modelFP.model.save_pretrained('./saved_models/trained/marianmt_v2_FP_en-sk_model
 modelFP.tokenizer.save_pretrained('./saved_models/trained/marianmt_v2_FP_en-sk_tokenizer',push_to_hub=False)
 
 
-training_argsEval = {'no_cuda': False, 'fp16': False, 'per_device_eval_batch_size': 4, 'predict_with_generate': True}
+training_argsEval = {'no_cuda': False, 'fp16': False, 'per_device_eval_batch_size': batch_size, 'predict_with_generate': True}
 pipeEval = Pipeline(Scenario.EVAL, model=modelFP, dataset_eval=train,
                     training_args=training_argsEval)
 print("BLEU on FP cpu")
